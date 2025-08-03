7497cd6e88813c5e017be09506483b6395a615d6
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-08-03 15:30:58 -0500

    feat: :fire: changes

--- infra/main.tf
+++ infra/main.tf
@@ -149,9 +149,9 @@ resource "aws_eip" "pr4_eip" {
   instance = aws_instance.pr4_ec2.id
 }
 
-# --------------------------
+# ---------------------------
 # RDS PostgreSQL
-# --------------------------
+# ---------------------------
 resource "aws_db_subnet_group" "pr4_db_subnet" {
   name       = "pr4-db-subnet-group"
   subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
