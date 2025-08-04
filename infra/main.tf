190fcfaeb840be2d8054f5608373ed724c429124
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-08-03 19:35:03 -0500

    feat: :fire:

--- infra/main.tf
+++ infra/main.tf
@@ -198,16 +198,6 @@ resource "aws_db_instance" "pr4_rds" {
   publicly_accessible    = false
 }
 
-# =========================================================
-# Outputs
-# =========================================================
-output "ec2_public_ip" {
-  value = aws_eip.pr4_eip.public_ip
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.pr4_rds.address
-}
 
 
 
